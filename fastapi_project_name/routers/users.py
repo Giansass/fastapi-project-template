edac2870da47b4375ed3e2deb742c9c5fd8268be
Giansanti Andrea (DS&G) <andrea.giansanti@enel.com>
2024-10-01 10:20:54 +0200

    added status code in routers/users and moved users schema in schemas

--- fastapi_project_name/routers/users.py
+++ fastapi_project_name/routers/users.py
@@ -1,19 +1,11 @@
 """An example of router used to retrieve username"""
 
-from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 
 from ..dependencies import get_token_header
-
-
-class UserInfo(BaseModel):
-    """UserName data model"""
-
-    user: str
-    name: str
-    surname: str
-    age: int | None = None
-
+from ..internal.schemas.users import UserInfo
 
 # Router definition
 router = APIRouter(
@@ -27,25 +19,30 @@ router = APIRouter(
 granted_usernames = {"andrea.giansanti": {"name": "Andrea", "surname": "Giansanti", "age": 34}}
 
 
-@router.get("/")
+@router.get("/", status_code=status.HTTP_200_OK)
 async def read_items():
     """Get all users"""
     return granted_usernames
 
 
-@router.get("/{username}")
-async def read_user(username: str):
+@router.get("/read_usernames/", status_code=status.HTTP_200_OK)
+async def read_user(username: Annotated[str | None, Query(max_length=50)] = None):
     """Return the required user"""
     if username not in granted_usernames:
-        raise HTTPException(status_code=404, detail="User not found")
-    return {
-        "name": granted_usernames[username]["name"],
-        "surname": granted_usernames[username]["surname"],
-        "age": granted_usernames[username]["age"],
-    }
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    return (
+        {
+            "name": granted_usernames[username]["name"],
+            "surname": granted_usernames[username]["surname"],
+            "age": granted_usernames[username]["age"],
+        }
+        if username is not None
+        else granted_usernames
+    )
 
 
-@router.post("/add_username")
+@router.post("/add_username", status_code=status.HTTP_201_CREATED)
 async def add_username(userinfo: UserInfo):
     """Add the required user"""
     granted_usernames[userinfo.user] = {
@@ -56,11 +53,11 @@ async def add_username(userinfo: UserInfo):
     return {"message": f"Username {userinfo.user} added"}
 
 
-@router.put("/update_username")
+@router.put("/update_username", status_code=status.HTTP_202_ACCEPTED)
 async def update_name(userinfo: UserInfo):
     """Update the required user"""
     if userinfo.user not in granted_usernames:
-        raise HTTPException(status_code=400, detail="Username not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
     granted_usernames[userinfo.user] = {
         "name": userinfo.name,
@@ -70,11 +67,11 @@ async def update_name(userinfo: UserInfo):
     return {"message": f"Username {userinfo.user} updated"}
 
 
-@router.delete("/delete_username/{username}")
+@router.delete("/delete_username/{username}", status_code=status.HTTP_202_ACCEPTED)
 async def delete(username: str):
     """Delete the required user"""
     if username not in granted_usernames:
-        raise HTTPException(status_code=400, detail="Username not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
     del granted_usernames[username]
     return {"message": f"Username {username} deleted"}
