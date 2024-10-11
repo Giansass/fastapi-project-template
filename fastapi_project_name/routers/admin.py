a0681cfa48564a72759c5af5d23477484375f609
Giansanti Andrea (DS&G) <andrea.giansanti@enel.com>
2024-10-11 15:52:23 +0200

    Working on admin.py

--- fastapi_project_name/routers/admin.py
+++ fastapi_project_name/routers/admin.py
@@ -1,37 +1,68 @@
 """aaa"""
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
 
-from fastapi import APIRouter
-from fastapi.security import OAuth2PasswordBearer
+import jwt
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
 
-from ..internal.utils.utils import SecretKeyGenerator
+from ..internal.utils.secret_key_generator import SecretKeyGenerator
 
-# # Parameters
-# SECRET_KEY = "93a5169cecdc311c4b1557213082083b"  # openssl rand -hex 16
-# ALGORITHM = "HS256"
-# ACCESS_TOKEN_EXPIRE_MINUTES = 30
+# Parameters
+SECRET_KEY = "93a5169cecdc311c4b1557213082083b"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Secret Key definition
+secret_key_generator = SecretKeyGenerator()
+secret_key = secret_key_generator.main()
 
 
-secret_key = SecretKeyGenerator()
+class Token(BaseModel):
+    """aaa"""
+
+    access_token: str
+    token_type: str
 
 
+class TokenData(BaseModel):
+    """aaa"""
+
+    username: str | None = None
+
+
+class User(BaseModel):
+    """aaa"""
+
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    """aaa"""
+
+    hashed_password: str
+
+
+# User DB
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
+    }
 }
 
 # Router definition
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 router = APIRouter(
     prefix="/admin",
     tags=["admin"],
@@ -40,44 +71,104 @@ router = APIRouter(
 )
 
 
-def fake_hash_password(password: str):
+def verify_password(plain_password, hashed_password):
     """aaa"""
-    return "fakehashed" + password
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-# Add OAUTH2 authentication method
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+def get_password_hash(password):
+    """aaa"""
+    return pwd_context.hash(password)
 
 
-# class User(BaseModel):
-#     """aaa"""
-#
-#     username: str
-#     email: str | None = None
-#     full_name: str | None = None
-#     disabled: bool | None = None
-#
-#
-# class UserInDB(User):
-#     """aaa"""
-#
-#     hashed_password: str
-#
-#
-# def get_user(db, username: str):
-#     """aaa"""
-#     if username in db:
-#         user_dict = db[username]
-#         return UserInDB(**user_dict)
-#
-#
-# def fake_decode_token(token):
-#     """aaa"""
-#     user = get_user(fake_users_db, token)
-#     return user
-#
-#
-# @router.get("/admin/")
-# async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-#     """aaa"""
-#     return {"token": token}
+def get_user(db, username: str | None = None):
+    """aaa"""
+    user = None
+    if username in db:
+        user_dict = db[username]
+        user = UserInDB(**user_dict)
+    return user
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    """aaa"""
+    user = get_user(fake_db, username)
+    if not user or not verify_password(password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """aaa"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    """aaa"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError as exc:
+        raise credentials_exception from exc
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    """aaa"""
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    """aaa"""
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username},
+        expires_delta=access_token_expires,
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@router.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    """aaa"""
+    return current_user
+
+
+@router.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    """aaa"""
+    return [{"item_id": "Foo", "owner": current_user.username}]
